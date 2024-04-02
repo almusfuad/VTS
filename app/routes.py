de5a1e15c48ebaf7ea2c486c2946b042457dd894
almusfuad <almusfuad@gmail.com>
2024-04-03 01:55:41 +0600

    searching operation worked

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,15 @@
-from fastapi import APIRouter, HTTPException, Depends
-from db import execute_post_query, execute_get_query
+from fastapi import APIRouter, HTTPException, Depends, Path
+from fastapi.responses import JSONResponse
 from auth import get_password_hash, verify_password, generate_access_token
 from models import User, LoginInput, Movie, Rating
 import mysql.connector
+from db import execute_post_query, execute_get_query
 
 user_router = APIRouter()
 movie_router = APIRouter()
 
 
-@user_router.post("/user/register/")
+@user_router.post("/user/register/", tags=["User"])
 def register_user(user: User):
       hashed_password = get_password_hash(user.password)
       
@@ -25,7 +26,7 @@ def register_user(user: User):
       
 
 
-@user_router.post("/user/login/")
+@user_router.post("/user/login/", tags=["User"])
 def login_user(input_data: LoginInput):
       try:
             query = "SELECT password FROM movie_rating.users WHERE email = %s"
@@ -51,7 +52,7 @@ def login_user(input_data: LoginInput):
             raise HTTPException(status_code=500, detail=f"Failed to login user: {str(e)}")
       
       
-@movie_router.post("/movie/")
+@movie_router.post("/movie/", tags=["Movie"])
 def add_movie(movie: Movie):
       try:
             query = """
@@ -72,7 +73,7 @@ def add_movie(movie: Movie):
             raise HTTPException(status_code=500, detail=f"Failed to add movie: {str(e)}")
       
       
-@movie_router.get("/movie/")
+@movie_router.get("/movie/", tags=["Movie"])
 def get_movies():
       try:
             query = "SELECT * FROM movie_rating.movies"
@@ -97,7 +98,38 @@ def get_movies():
             raise HTTPException(status_code=500, detail=f"Failed to get movies: {str(e)}")
       
 
-@movie_router.post("/rating/")
+
+@movie_router.get("/movie/{movie_name}", tags=["Movie"])
+def search_movie(movie_name: str = Path(..., title = "Movie Name", description="Name of the movie to search for")):
+      try:
+            query = """ 
+                  SELECT * FROM movie_rating.movies WHERE name LIKE %s
+            """ 
+            
+            result = execute_get_query(query, (f"%{movie_name}%", ))  # added % to enable partial match
+            
+            if not result:
+                  raise HTTPException(status_code=404, detail="No movies found with the provided name")
+            
+            movies = []
+            for row in result:
+                  movie = {
+                        "id": row[0],
+                        "name": row[1],
+                        "genre": row[2],
+                        "rating": row[3],
+                        "release_date": row[4].strftime("%d-%m-%Y")
+                  }
+                  
+                  movies.append(movie)
+            return movies
+      except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Failed to search for movies: {str(e)}")
+                  
+      
+
+
+@movie_router.post("/rating/", tags=["Rating"])
 def add_rating(rating: Rating):
       try:
             query = """
@@ -112,7 +144,7 @@ def add_rating(rating: Rating):
       
 
 
-@movie_router.get("/rating/")
+@movie_router.get("/rating/", tags=["Rating"])
 def all_rating(rating: Rating):
       try:
             query = '''
